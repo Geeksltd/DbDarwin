1ae246db16f73c5890428af385b592ed45c97c8a
Hatef Rostamkhani <10432308+Hatef-Rostamkhani@users.noreply.github.com>
2018-10-10 16:56:01 +0330

    Test Serialize Table and Columns
    I create new class for table and column can be serlizable

---
+++ Source/DbDarwin/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace DbDarwin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                var first = args.First();
+                if (!string.IsNullOrEmpty(first) && first.ToLower() == "extract-schema")
+                {
+                    Console.WriteLine("Start Extract Schema...");
+                    System.Data.SqlClient.SqlConnection sql = new System.Data.SqlClient.SqlConnection("Data Source=EPIPC;Initial Catalog=AdventureWorks2012;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+                    sql.Open();
+                    var dt = sql.GetSchema("Tables");
+
+
+                    SqlDataAdapter da = new SqlDataAdapter();
+                    da.SelectCommand = new SqlCommand();
+                    da.SelectCommand.Connection = sql;
+
+
+                    foreach (DataRow r in dt.Rows)
+                    {
+                        Console.WriteLine(r["TABLE_NAME"]);
+                        var tableName = r["TABLE_NAME"];
+
+                        da.SelectCommand.CommandText = "select * FROM [Sales].[Store]";// + tableName;
+                        DataTable dt1 = new DataTable("Store");
+                        da.Fill(dt1);
+
+
+
+                        DbDarwin.SchemaXML.Table myDt = new SchemaXML.Table()
+                        {
+                            Name = dt1.TableName
+
+                        };
+
+
+                        var ser = new XmlSerializer(typeof(SchemaXML.Table));
+                        StringWriter sw2 = new StringWriter();
+                        ser.Serialize(sw2, myDt);
+                        var xml = sw2.ToString();
+                        File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "\\schema" + DateTime.Now.Ticks + ".xml", xml);
+                        //Console.WriteLine(xml);
+                        //StringWriter sw = new StringWriter();
+                        //dt1.WriteXml(sw, XmlWriteMode.DiffGram);
+                        //var xml = sw.ToString();
+                        //Console.WriteLine(xml);
+                        //File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "\\schema" + DateTime.Now.Ticks + ".xml", xml);
+                        break;
+                    }
+
+                    //MemoryStream ms = new MemoryStream();
+                    //dt.WriteXml(ms, XmlWriteMode.IgnoreSchema);
+                    //ms.Seek(0, SeekOrigin.Begin);
+                    //StreamReader sr = new StreamReader(ms);
+                    //string xml = sr.ReadToEnd();
+                    //ms.Close();
+
+                    //     File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "\\schema" + DateTime.Now.Ticks + ".xml", xml);
+
+                    //Data Source=EPIPC;Initial Catalog=MSharp.Mvc2.Temp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ Source/DbDarwin/TableSchema.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace DbDarwin.SchemaXML
+{
+
+
+    [XmlRoot("Table")]
+    public class Table
+    {
+        [XmlAttribute]
+        public string Name { get; set; }
+
+        public Column Column { get; set; }
+
+        // The XmlArray attribute changes the XML element name  
+        // from the default of "OrderedItems" to "Items".  
+        //[XmlArray("Items")]
+        //public OrderedItem[] OrderedItems;
+        //public decimal SubTotal;
+        //public decimal ShipCost;
+        //public decimal TotalCost;
+    }
+
+
+    [Serializable]
+    public class Column
+    {
+
+        [DefaultValue(false)]
+        public bool ReadOnly { get; set; }
+
+        [DefaultValue("")]
+        public string Prefix { get; set; }
+
+        [Browsable(false)]
+        public int Ordinal { get; }
+
+        public string Namespace { get; set; }
+
+        [DefaultValue(-1)]
+        public int MaxLength { get; set; }
+
+        [Browsable(false)]
+        public PropertyCollection ExtendedProperties { get; set; }
+
+        [DefaultValue("")]
+        [RefreshProperties(RefreshProperties.All)]
+        public string Expression { get; set; }
+
+        public object DefaultValue { get; set; }
+
+        [DefaultValue(DataSetDateTime.UnspecifiedLocal)]
+        public DataSetDateTime DateTimeMode { get; set; }
+
+        [DefaultValue(typeof(string))]
+        public Type DataType { get; set; }
+
+        [DefaultValue("")]
+        public string ColumnName { get; set; }
+
+        [DefaultValue(1)]
+        public long AutoIncrementStep { get; set; }
+
+        public string Caption { get; set; }
+
+        //[Browsable(false)]
+        //public DataTable Table { get; }
+
+        [DefaultValue(0)]
+        public long AutoIncrementSeed { get; set; }
+
+        [DefaultValue(false)]
+        [RefreshProperties(RefreshProperties.All)]
+        public bool AutoIncrement { get; set; }
+
+        [DefaultValue(true)]
+        public bool AllowDBNull { get; set; }
+
+        [DefaultValue(MappingType.Element)]
+        public virtual MappingType ColumnMapping { get; set; }
+
+        [DefaultValue(false)]
+        public bool Unique { get; set; }
+    }
+}
