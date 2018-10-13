a27f251e85ce3d411e7b7f427334f1e56e082b1b
Hatef Rostamkhani <hatef.rostamkhani@gmail.com>
2018-10-13 20:05:41 +0330

    Test Load XML and test compare two object

--- Source/DbDarwin.UnitTestProject/UnitTest1.cs
+++ Source/DbDarwin.UnitTestProject/UnitTest1.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
 using DbDarwin.Service;
+using KellermanSoftware.CompareNetObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DbDarwin.UnitTestProject
@@ -22,14 +24,29 @@ namespace DbDarwin.UnitTestProject
         [TestMethod]
         public void TestLoadXML()
         {
-            //XmlSerializer serializer = new XmlSerializer(typeof(List<DbDarwin.Model.Table>));
+            XmlSerializer serializer = new XmlSerializer(typeof(List<DbDarwin.Model.Table>));
+            List<DbDarwin.Model.Table> result1 = null;
+            List<DbDarwin.Model.Table> result2 = null;
+            using (var reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\" + "xml1.xml"))
+                result1 = (List<DbDarwin.Model.Table>)serializer.Deserialize(reader);
+            using (var reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "\\" + "xml2.xml"))
+                result2 = (List<DbDarwin.Model.Table>)serializer.Deserialize(reader);
 
-            //StringWriter sw2 = new StringWriter();
-            //serializer.Deserialize(sw2, tables);
-            //using (TextReader reader = new StringReader(xmlResult))
-            //{
-            //    GetOrdersResponse result = (GetOrdersResponse)serializer.Deserialize(reader);
-            //}
+            CompareLogic compareLogic = new CompareLogic();
+
+            var result = compareLogic.Compare(result1, result2);
+
+            if (!result.AreEqual)
+            {
+                foreach (var r in result.Differences)
+                {
+                    Console.WriteLine(r.Object1TypeName + ":" + r.Object1Value);
+                    Console.WriteLine(r.Object2TypeName + ":" + r.Object2Value);
+                }
+            }
+
+
+            Assert.IsTrue(true);
         }
 
     }
