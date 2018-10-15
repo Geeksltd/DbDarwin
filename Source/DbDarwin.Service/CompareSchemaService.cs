724928132c5bef1b39ea157fbd537aacdb2e17e2
Hatef Rostamkhani <hatef.rostamkhani@gmail.com>
2018-10-15 13:47:55 +0330

    Change the structure of the code in the project console app can readable code and create  reusable methods

--- Source/DbDarwin.Service/CompareSchemaService.cs
+++ Source/DbDarwin.Service/CompareSchemaService.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Xml;
 using System.Xml.Serialization;
 using System.Xml.XPath;
+using DbDarwin.Model;
 using DbDarwin.Model.Schema;
 using KellermanSoftware.CompareNetObjects;
 
@@ -14,107 +15,141 @@ namespace DbDarwin.Service
     public class CompareSchemaService
     {
 
-        public static void StartCompare(string currentFileName, string newSchema, string output)
+        /// <summary>
+        /// compare two xml file and create diff xml file
+        /// </summary>
+        /// <param name="currentFileName"></param>
+        /// <param name="newSchema"></param>
+        /// <param name="output"></param>
+        public static bool StartCompare(string currentFileName, string newSchema, string output)
         {
-            XmlSerializer serializer = new XmlSerializer(typeof(List<DbDarwin.Model.Table>));
-            List<DbDarwin.Model.Table> result1 = null;
-            List<DbDarwin.Model.Table> result2 = null;
-            using (var reader = new StreamReader(currentFileName))
-                result1 = (List<DbDarwin.Model.Table>)serializer.Deserialize(reader);
-            using (var reader = new StreamReader(newSchema))
-                result2 = (List<DbDarwin.Model.Table>)serializer.Deserialize(reader);
-
-
-            //XmlAttributeOverrides sampleClassAttributes = new XmlAttributeOverrides();
-            XmlDocument doc = new XmlDocument();
-            XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
-            doc.AppendChild(docNode);
-            // XmlSerializerNamespaces ns1 = new XmlSerializerNamespaces();
-            // ns1.Add("xmlns", "http://www.w3.org/2001/XMLSchema");
-            XmlElement ArrayOfTable = doc.CreateElement("ArrayOfTable");
-            var emptyNamepsaces = new XmlSerializerNamespaces(new[] {
-                XmlQualifiedName.Empty
-            });
-
-
-            foreach (var r1 in result1)
+            try
             {
-                var findedTable = result2.FirstOrDefault(x => x.Name == r1.Name);
-                if (findedTable == null)
+                XmlSerializer serializer = new XmlSerializer(typeof(List<Table>));
+                List<Table> result1 = null;
+                List<Table> result2 = null;
+                using (var reader = new StreamReader(currentFileName))
+                    result1 = (List<Table>)serializer.Deserialize(reader);
+                using (var reader = new StreamReader(newSchema))
+                    result2 = (List<Table>)serializer.Deserialize(reader);
+
+
+                XmlDocument doc = new XmlDocument();
+                XmlNode docNode = doc.CreateXmlDeclaration("1.0", "UTF-8", null);
+                doc.AppendChild(docNode);
+                XmlElement ArrayOfTable = doc.CreateElement("ArrayOfTable");
+                var emptyNamepsaces = new XmlSerializerNamespaces(new[] {
+                    XmlQualifiedName.Empty
+                });
+
+
+                foreach (var r1 in result1)
                 {
-                    // Must Delete
+                    var findedTable = result2.FirstOrDefault(x => x.Name == r1.Name);
+                    if (findedTable == null)
+                    {
+                        // Must Delete
+                    }
+                    else
+                    {
+                        XmlElement root = doc.CreateElement("Table");
+                        var name = doc.CreateAttribute("Name");
+                        name.Value = r1.Name;
+                        root.Attributes.Append(name);
+
+
+                        XmlElement add = doc.CreateElement("add");
+                        var naviagetorAdd = add.CreateNavigator();
+
+                        XmlElement removeColumn = doc.CreateElement("remove");
+                        XPathNavigator naviagetorRemove = removeColumn.CreateNavigator();
+
+
+
+                        GenerateDifference<Column>(doc, root, r1.Column, findedTable.Column, naviagetorAdd, naviagetorRemove, "COLUMN_NAME");
+                        GenerateDifference<Index>(doc, root, r1.Index, findedTable.Index, naviagetorAdd, naviagetorRemove, "name");
+                        GenerateDifference<REFERENTIAL_CONSTRAINTS>(doc, root, r1.ForeignKey, findedTable.ForeignKey, naviagetorAdd, naviagetorRemove, "CONSTRAINT_NAME");
+
+                        if (add.HasChildNodes)
+                            root.AppendChild(add);
+
+                        if (removeColumn.HasChildNodes)
+                            root.AppendChild(removeColumn);
+                        ArrayOfTable.AppendChild(root);
+                    }
                 }
-                else
-                {
-                    XmlElement root = doc.CreateElement("Table");
-                    var name = doc.CreateAttribute("Name");
-                    name.Value = r1.Name;
-                    root.Attributes.Append(name);
-
-
-                    XmlElement add = doc.CreateElement("add");
-                    var naviagetorAdd = add.CreateNavigator();
-
-                    XmlElement removeColumn = doc.CreateElement("remove");
-                    XPathNavigator naviagetorRemove = removeColumn.CreateNavigator();
-
-
-
-                    GenerateDifference<Column>(doc, root, r1.Column, findedTable.Column, naviagetorAdd, naviagetorRemove, "COLUMN_NAME");
-                    GenerateDifference<Index>(doc, root, r1.Index, findedTable.Index, naviagetorAdd, naviagetorRemove, "name");
-                    GenerateDifference<REFERENTIAL_CONSTRAINTS>(doc, root, r1.ForeignKey, findedTable.ForeignKey, naviagetorAdd, naviagetorRemove, "CONSTRAINT_NAME");
-
-                    if (add.HasChildNodes)
-                        root.AppendChild(add);
-
-                    if (removeColumn.HasChildNodes)
-                        root.AppendChild(removeColumn);
-                    ArrayOfTable.AppendChild(root);
-                }
+                doc.AppendChild(ArrayOfTable);
+                doc.Save(output);
+                //File.WriteAllText(path, xml);
+                Console.WriteLine("Saving To xml");
             }
-            doc.AppendChild(ArrayOfTable);
-            doc.Save(output);
-            //File.WriteAllText(path, xml);
-            Console.WriteLine("Saving To xml");
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+                return false;
+            }
+            finally
+            {
+                GC.Collect();
+            }
+
+            return true;
         }
 
+        /// <summary>
+        /// compare objects
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="doc"></param>
+        /// <param name="root"></param>
+        /// <param name="currentList"></param>
+        /// <param name="newList"></param>
+        /// <param name="naviagetorAdd"></param>
+        /// <param name="naviagetorRemove"></param>
+        /// <param name="properyCheck"></param>
         public static void GenerateDifference<T>(XmlDocument doc, XmlElement root,
-            List<T> list, List<T> findedTable,
+            List<T> currentList, List<T> newList,
             XPathNavigator naviagetorAdd, XPathNavigator naviagetorRemove, string properyCheck)
         {
             var emptyNamespaces = new XmlSerializerNamespaces(new[] {
                 XmlQualifiedName.Empty
             });
 
+            #region Detect new sql object like as INDEX , Column , REFERENTIAL_CONSTRAINTS 
+
             List<T> mustAdd = new List<T>();
             if (typeof(T) == typeof(Column))
             {
 
-                var tempAdd = findedTable.Cast<Column>()
-                    .Where(x => !list.Cast<Column>().Select(c => c.COLUMN_NAME).ToList().Contains(x.COLUMN_NAME)).ToList();
+                var tempAdd = newList.Cast<Column>()
+                    .Where(x => !currentList.Cast<Column>().Select(c => c.COLUMN_NAME).ToList().Contains(x.COLUMN_NAME)).ToList();
                 mustAdd = (List<T>)Convert.ChangeType(tempAdd, typeof(List<T>));
             }
             else if (typeof(T) == typeof(Index))
             {
-                var tempAdd = findedTable.Cast<Index>()
-                    .Where(x => !list.Cast<Index>().Select(c => c.name).ToList().Contains(x.name)).ToList();
+                var tempAdd = newList.Cast<Index>()
+                    .Where(x => !currentList.Cast<Index>().Select(c => c.name).ToList().Contains(x.name)).ToList();
                 mustAdd = (List<T>)Convert.ChangeType(tempAdd, typeof(List<T>));
             }
             else if (typeof(T) == typeof(REFERENTIAL_CONSTRAINTS))
             {
-                var tempAdd = findedTable.Cast<REFERENTIAL_CONSTRAINTS>()
-                    .Where(x => !list.Cast<REFERENTIAL_CONSTRAINTS>().Select(c => c.CONSTRAINT_NAME).ToList().Contains(x.CONSTRAINT_NAME)).ToList();
+                var tempAdd = newList.Cast<REFERENTIAL_CONSTRAINTS>()
+                    .Where(x => !currentList.Cast<REFERENTIAL_CONSTRAINTS>().Select(c => c.CONSTRAINT_NAME).ToList().Contains(x.CONSTRAINT_NAME)).ToList();
                 mustAdd = (List<T>)Convert.ChangeType(tempAdd, typeof(List<T>));
             }
 
+            #endregion
 
-            foreach (T column in mustAdd)
+            // Add new objects to xml
+            foreach (T sqlObject in mustAdd)
             {
                 using (var writer = naviagetorAdd.AppendChild())
                 {
-                    var serializer1 = new XmlSerializer(column.GetType());
+                    var serializer1 = new XmlSerializer(sqlObject.GetType());
                     writer.WriteWhitespace("");
-                    serializer1.Serialize(writer, column, emptyNamespaces);
+                    serializer1.Serialize(writer, sqlObject, emptyNamespaces);
                     writer.Close();
                 }
             }
@@ -127,7 +162,8 @@ namespace DbDarwin.Service
                 Config = { MaxDifferences = Int32.MaxValue }
             };
 
-            foreach (T c1 in list)
+            // Detect Sql Objects Changes
+            foreach (T c1 in currentList)
             {
                 XmlElement column = doc.CreateElement(typeof(T).Name);
 
@@ -135,17 +171,17 @@ namespace DbDarwin.Service
 
                 if (typeof(T) == typeof(Column))
                 {
-                    var found = findedTable.Cast<Column>().FirstOrDefault(x => x.COLUMN_NAME == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
+                    var found = newList.Cast<Column>().FirstOrDefault(x => x.COLUMN_NAME == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
                     foundObject = (T)Convert.ChangeType(found, typeof(T));
                 }
                 else if (typeof(T) == typeof(Index))
                 {
-                    var found = findedTable.Cast<Index>().FirstOrDefault(x => x.name == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
+                    var found = newList.Cast<Index>().FirstOrDefault(x => x.name == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
                     foundObject = (T)Convert.ChangeType(found, typeof(T));
                 }
                 else if (typeof(T) == typeof(REFERENTIAL_CONSTRAINTS))
                 {
-                    var found = findedTable.Cast<REFERENTIAL_CONSTRAINTS>().FirstOrDefault(x => x.CONSTRAINT_NAME == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
+                    var found = newList.Cast<REFERENTIAL_CONSTRAINTS>().FirstOrDefault(x => x.CONSTRAINT_NAME == c1.GetType().GetProperty(properyCheck).GetValue(c1).ToString());
                     foundObject = (T)Convert.ChangeType(found, typeof(T));
                 }
 
--- Source/DbDarwin/Program.cs
+++ Source/DbDarwin/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using DbDarwin.Service;
 
@@ -13,37 +14,62 @@ namespace DbDarwin
             if (argList.Count > 0)
             {
                 var first = args.First();
-                if (!string.IsNullOrEmpty(first) && first.ToLower() == "extract-schema")
+                if (!string.IsNullOrEmpty(first))
                 {
-                    Console.WriteLine("Start Extract Schema...");
-
-                    // Read -connect parameter
-                    var index = argList.IndexOf("-connect");
-                    if (index == -1)
+                    if (first.ToLower() == "extract-schema")
                     {
-                        Console.WriteLine("-connect parameter is requirement");
-                        Console.ReadLine();
-                        return;
+                        if (ValidateArguamentExtaractSchema(argList, out var connection, out var outputFile))
+                            ExtractSchemaService.ExtractSchema(connection, outputFile);
                     }
-
-                    string connection = string.Empty;
-                    if (argList.Count > index + 1)
-                        connection = argList[index + 1];
-
-                    // Read -out parameter
-                    var indexOut = argList.IndexOf("-out");
-                    if (indexOut == -1)
+                    else if (first.ToLower() == "generate-diff")
                     {
-                        Console.WriteLine("-out parameter is requirement");
-                        Console.ReadLine();
-                        return;
+                        if (ValidateArguamentGenerateDiff(argList, out var currentFile, out var newSchemaFile,
+                            out var outputFile))
+                            CompareSchemaService.StartCompare(currentFile, newSchemaFile, outputFile);
                     }
-                    string outFile = string.Empty;
-                    if (argList.Count > indexOut + 1)
-                        outFile = argList[indexOut + 1];
-                    ExtractSchemaService.ExtractSchema(connection, outFile);
                 }
             }
+        }
+
+        private static bool ValidateArguamentExtaractSchema(List<string> argList, out string connection, out string outputFile)
+        {
+            Console.WriteLine("Start Extract Schema...");
+            // Read -connect parameter
+            connection = ReadArgument("-connect", argList, "-connect parameter is requirement");
+            // Read -out parameter
+            outputFile = ReadArgument("-out", argList, "-out parameter is requirement");
+
+            return !string.IsNullOrEmpty(connection) && !string.IsNullOrEmpty(outputFile);
+
+        }
+
+        private static bool ValidateArguamentGenerateDiff(List<string> argList, out string currentFile, out string newSchemaFile, out string outputFile)
+        {
+            Console.WriteLine("Start generate the differences...");
+            // Read -from parameter
+            currentFile = ReadArgument("-from", argList, "-from parameter is requirement");
+            // Read -to parameter
+            newSchemaFile = ReadArgument("-to", argList, "-to parameter is requirement");
+            // Read -out parameter
+            outputFile = ReadArgument("-out", argList, "-out parameter is requirement");
+
+            return !string.IsNullOrEmpty(currentFile) && !string.IsNullOrEmpty(newSchemaFile) &&
+                   !string.IsNullOrEmpty(outputFile);
+        }
+
+        private static string ReadArgument(string argument, List<string> argList, string message)
+        {
+            var index = argList.IndexOf(argument);
+            if (index == -1)
+            {
+                Console.WriteLine(message);
+                Console.ReadLine();
+                return string.Empty;
+            }
+
+            if (argList.Count > index + 1)
+                return argList[index + 1];
+            return string.Empty;
         }
     }
 }
