5705124e4ff18bfad2e364c459dafc159c9b9d78
Hatef Rostamkhani <hatef.rostamkhani@gmail.com>
2018-11-07 21:56:43 +0330

    set CollationName default value to SQL_Latin1_General_CP1_CI_AS set DELETE_RULE default value to NO ACTION set UPDATE_RULE default value to NO ACTION set RefTableSchema dafault value dbo remove Ref_ORDINAL_POSITION and ORDINAL_POSITION and MatchOption from ForeignKey

--- Source/DbDarwin.Model/Schema/ForeignKey.cs
+++ Source/DbDarwin.Model/Schema/ForeignKey.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Xml;
 using System.Xml.Serialization;
+using Olive;
 
 namespace DbDarwin.Model.Schema
 {
@@ -11,6 +12,13 @@ namespace DbDarwin.Model.Schema
 
     public class ForeignKey
     {
+        public ForeignKey()
+        {
+            UPDATE_RULE = "NO ACTION";
+            DELETE_RULE = "NO ACTION";
+            Ref_TABLE_SCHEMA = "dbo";
+        }
+
         [XmlAttribute(AttributeName = "Set-Name")]
         public string SetName { get; set; }
 
@@ -35,14 +43,23 @@ namespace DbDarwin.Model.Schema
         [XmlAttribute(AttributeName = "UniqueConstraintName")]
         public string UNIQUE_CONSTRAINT_NAME { get; set; }
 
+        [XmlIgnore]
         [XmlAttribute(AttributeName = "MatchOption")]
         public string MATCH_OPTION { get; set; }
 
         [XmlAttribute(AttributeName = "UpdateRule")]
         public string UPDATE_RULE { get; set; }
+        public bool ShouldSerializeUPDATE_RULE()
+        {
+            return UPDATE_RULE.HasValue() && UPDATE_RULE.ToLower() != "NO ACTION".ToLower();
+        }
 
         [XmlAttribute(AttributeName = "DeleteRule")]
         public string DELETE_RULE { get; set; }
+        public bool ShouldSerializeDELETE_RULE()
+        {
+            return DELETE_RULE.HasValue() && DELETE_RULE.ToLower() != "NO ACTION".ToLower();
+        }
 
         [XmlAttribute(AttributeName = "TableName")]
         public string TABLE_NAME { get; set; }
@@ -50,11 +67,16 @@ namespace DbDarwin.Model.Schema
         [XmlAttribute(AttributeName = "ColumnName")]
         public string COLUMN_NAME { get; set; }
 
+        [XmlIgnore]
         [XmlAttribute(AttributeName = "OrdinalPosition")]
         public int ORDINAL_POSITION { get; set; }
 
         [XmlAttribute(AttributeName = "RefTableSchema")]
         public string Ref_TABLE_SCHEMA { get; set; }
+        public bool ShouldSerializeRef_TABLE_SCHEMA()
+        {
+            return Ref_TABLE_SCHEMA.HasValue() && Ref_TABLE_SCHEMA.ToLower() != "dbo".ToLower();
+        }
 
         [XmlAttribute(AttributeName = "RefTableName")]
         public string Ref_TABLE_NAME { get; set; }
@@ -62,6 +84,7 @@ namespace DbDarwin.Model.Schema
         [XmlAttribute(AttributeName = "RefColumnName")]
         public string Ref_COLUMN_NAME { get; set; }
 
+        [XmlIgnore]
         [XmlAttribute(AttributeName = "RefOrdinalPosition")]
         public int Ref_ORDINAL_POSITION { get; set; }
     }
